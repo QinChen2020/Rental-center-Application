cb9efab1be0a00b72e5e55d2206d5c0619f91860
QinChen2020 <83799497+QinChen2020@users.noreply.github.com>
2021-09-06 00:20:33 +1200

    update

--- app/src/main/java/rental_center_application/com/adapter/LowestPriceAdapter.java
+++ app/src/main/java/rental_center_application/com/adapter/LowestPriceAdapter.java
@@ -1,43 +1,49 @@
 package rental_center_application.com.adapter;
 
+
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
+import java.util.Objects;
 
 import rental_center_application.com.model.Lowestprice;
 
 public class LowestPriceAdapter extends RecyclerView.Adapter<LowestPriceAdapter.LowestPriceViewHolder>{
-
     Context context;
     List<Lowestprice> LowestPriceList;
 
     @NonNull
     @Override
-    public LowestPriceViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public LowestPriceViewHolder onCreateViewHolder(@NonNull  viewGroup parent, int viewType) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public LowestPriceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull LowestPriceViewHolder lowestPriceViewHolder, int i) {
+    public void onBindViewHolder(@NonNull LowestPriceViewHolder holder, int position) {
 
     }
 
     @Override
-    public int getItemCount() {
-        return LowestPriceList.size();
+    public int getItemCount (){
+        return LowestPriceList.();
     }
 
-    public static class LowestPriceViewHolder extends RecyclerView.ViewHolder{
+    public static class LowestPriceViewHolder extends RecyclerView.ViewHolder {
 
-        public LowestPriceViewHolder(@NonNull View itemView){
-            super(itemView);
+        public LowestPriceViewHolder(@NonNull View itemView) {
+            super(Objects.requireNonNull(itemView));
+
         }
-
-
     }
+
 }
