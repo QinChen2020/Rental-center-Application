bc7b6a73e2dbe6c9d4cca97ad7ea88f334999afa
QinChen2020 <83799497+QinChen2020@users.noreply.github.com>
2021-09-19 10:56:02 +1200

    111

--- app/src/androidTest/java/com/example/rental_centre_app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/rental_centre_app/ExampleInstrumentedTest.java
@@ -1,25 +1,31 @@
 package com.example.rental_centre_app;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+<<<<<<< Updated upstream
 @RunWith(AndroidJUnit4.class)
 class ExampleInstrumentedTest {
+=======
+
+public class ExampleInstrumentedTest {
+>>>>>>> Stashed changes
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Context appContext;
+        appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.rental_centre_app", appContext.getPackageName());
     }
 }
