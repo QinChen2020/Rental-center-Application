7ea32b0b1037368ca3874fe5a920041b42c2c959
PatrickPei747 <754463323@qq.com>
2021-09-23 17:05:51 +1200

    update

--- app/src/main/java/com/example/rental_centre_app/Register.java
+++ app/src/main/java/com/example/rental_centre_app/Register.java
@@ -1,15 +1,22 @@
 package com.example.rental_centre_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 import com.example.rental_centre_app.utils.JdbcUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -19,6 +26,9 @@ public class Register extends AppCompatActivity {
 
     private Button register;
     private Button return_button;
+    private EditText email;
+    private EditText password;
+    private FirebaseAuth auth;
 
 
     @Override
@@ -27,6 +37,9 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         register = (Button) findViewById(R.id.register_button);
         return_button = (Button)findViewById(R.id.return_button);
+        email = findViewById(R.id.editText_email);
+        password = findViewById(R.id.editText_password);
+        auth = FirebaseAuth.getInstance();
         setListeners();
     }
     private void setListeners(){
@@ -39,33 +52,19 @@ public class Register extends AppCompatActivity {
 
         @Override
         public void onClick(View view) {
-            EditText usernameText = findViewById(R.id.editText_username);
-            String username = usernameText.getText().toString();
-            EditText passwordText = findViewById(R.id.editText_password);
-            String password = passwordText.getText().toString();
             Intent intent = null;
             switch (view.getId()){
                 case R.id.register_button:
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            int i = 0;
-                            Connection conn = JdbcUtil.getConnection();
-                            String sql = "insert into `user`(`username`,`password`)values(?,?)";
-                            try {
-                                PreparedStatement pst = conn.prepareStatement(sql);
-                                pst.setString(1,username);
-                                pst.setString(2,password);
-                                i = pst.executeUpdate();
-                                System.out.println(i);
-                                pst.close();
-                                conn.close();
-                            } catch (SQLException throwables) {
-                                throwables.printStackTrace();
-                            }
-                        }
-                    }).start();
-                    intent = new Intent(Register.this, Register_successful.class);
+                    String txt_email = email.getText().toString();
+                    String txt_password = password.getText().toString();
+                    if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
+                        Toast.makeText(Register.this,"Empty credentials!", Toast.LENGTH_SHORT).show();
+                    }else if (txt_password.length() < 6) {
+                        Toast.makeText(Register.this,"Password too short!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        registerUser(txt_email, txt_password);
+                        intent = new Intent(Register.this, Register_successful.class);
+                    }
                     break;
                 case R.id.return_button:
                     intent = new Intent(Register.this, MainPageActivity.class);
@@ -73,5 +72,18 @@ public class Register extends AppCompatActivity {
             }
             startActivity(intent);
         }
+    }
+
+    private void registerUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(Register.this,"Register successful", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(Register.this,"Register failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
