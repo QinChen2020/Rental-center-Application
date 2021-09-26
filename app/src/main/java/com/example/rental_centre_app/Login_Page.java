786d3e26868ce5a46066d37e518241a5691157bf
PatrickPei747 <754463323@qq.com>
2021-09-26 15:30:37 +1300

    update

--- app/src/main/java/com/example/rental_centre_app/Login_Page.java
+++ app/src/main/java/com/example/rental_centre_app/Login_Page.java
@@ -6,10 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login_Page extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
     private Button login;
     private Button return_button;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,11 @@ public class Login_Page extends AppCompatActivity {
         setContentView(R.layout.activity_login_page);
         login = findViewById(R.id.login_button);
         return_button = (Button)findViewById(R.id.return_button);
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+
+        auth = FirebaseAuth.getInstance();
+
         setListeners();
     }
     private void setListeners(){
@@ -31,15 +46,26 @@ public class Login_Page extends AppCompatActivity {
             Intent intent = null;
             switch (view.getId()){
                 case R.id.login_button:
+                    String txt_email = email.getText().toString();
+                    String txt_password = password.getText().toString();
+                    loginUser(txt_email,txt_password);
                     intent = new Intent(Login_Page.this,MainActivity.class);
+                    startActivity(intent);
                     break;
                 case R.id.return_button:
                     intent = new Intent(Login_Page.this, MainPageActivity.class);
+                    startActivity(intent);
                     break;
-
-
             }
-            startActivity(intent);
         }
+    }
+
+    private void loginUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(Login_Page.this, "login successful",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
